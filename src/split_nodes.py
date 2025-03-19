15a6ddc9ba4a2beba503cadb02765e2a0c7c6653
proff001 <thommhole@gmail.com>
2025-03-19 23:02:46 +0100

    Started on split_nodes_delimiter

---
+++ src/split_nodes.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+	new_nodes = []
+
+	for node in old_nodes:
+		split = node.text.split(delimiter)
+
+
+
+	return new_nodes
